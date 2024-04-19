938a7d5b9d14667195f21fed99a3cf674c1ec739
Paulo <paulomarcellor@gmail.com>
2024-04-19 09:51:48 -0300

    v1.1

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from tasks.models import dataTask, stepsTask
 
 class dataTaskSerializer(serializers.ModelSerializer):
+    days = serializers.SerializerMethodField()
     class Meta:
         model = dataTask
         fields = '__all__'
+    
+    def get_days(self, obj):
+        diferenca = obj.deadline - obj.start
+        return diferenca.days
 
 class stepsTaskSerializer(serializers.ModelSerializer):
     class Meta:
